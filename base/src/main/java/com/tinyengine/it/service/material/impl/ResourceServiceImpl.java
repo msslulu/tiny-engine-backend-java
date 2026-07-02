d46f8f438c33adb0ddd85d104328e024ada014db
msslulu <1484036491@qq.com>
2026-07-02 02:36:27 -0700

    fix:resourceUpload

--- base/src/main/java/com/tinyengine/it/service/material/impl/ResourceServiceImpl.java
+++ base/src/main/java/com/tinyengine/it/service/material/impl/ResourceServiceImpl.java
@@ -200,7 +200,13 @@ public class ResourceServiceImpl extends ServiceImpl<ResourceMapper, Resource> i
             resource.setThumbnailUrl(thumbnailUrl);
             resource.setThumbnailData(ImageThumbnailGenerator.createThumbnail(resource.getResourceData(), 200, 200));
         }
-
+        QueryWrapper<Resource> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("hash", resource.getHash());
+        // 接入租户系统需添加租户id查询
+        Resource resourceResult = this.baseMapper.selectOne(queryWrapper);
+        if (resourceResult != null) {
+            return resourceResult;
+        }
         int createResult = this.baseMapper.createResource(resource);
         if (createResult != 1) {
             throw new ServiceException(ExceptionEnum.CM002.getResultCode(), ExceptionEnum.CM002.getResultMsg());
